e27c0c435082328f37ac62b5b9980508ea5d59b5
Kris-Mikael Krister <kris-mikael.krister@objectware.no>
2011-11-07 13:57:55 +0100

    Added support for updating and delete viewings

--- app/controllers/Viewings.java
+++ app/controllers/Viewings.java
@@ -24,8 +24,16 @@ public class Viewings extends Controller {
     }
 
 	public static void update(String body) {
-		Viewing v = new Gson().fromJson(body, Viewing.class);
+		Viewing updatedViewing = new Gson().fromJson(body, Viewing.class);
+		Viewing v = Viewing.findById(updatedViewing.id);
+		v.address = updatedViewing.address;
 		v.save();
+		renderJSON(v);
+	}
+
+	public static void destroy(Long id) {
+		Viewing viewing = Viewing.findById(id);
+		viewing.delete();
 	}
     
 }
