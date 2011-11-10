2403ac92fb2ceb56ce39a25fdeb38723435a7da2
Pavel Fatrdla <pavel.fatrdla@iteraconsulting.no>
2011-11-10 12:29:28 +0100

    adding form to create new items

--- public/test.coffee
+++ public/test.coffee
@@ -3,22 +3,77 @@ class Viewing extends Backbone.Model
 class ViewingCollection extends Backbone.Collection
   model: Viewing
   url: "/viewings"
+
+class ViewingIndexView extends Backbone.View
+  setCollection: (@items) ->
+  
+  render: ->
+    form = new ViewingFormView()
+    list = new ViewingListView()
+    @renderPage(form, list)  
+    @
+    
+  renderPage: (form, list) ->
+    $("body").append(form.render().el)
+    $("body").append(list.renderList(@items).el)
+    
+  
+class ViewingFormView extends Backbone.View
+  template: _.template('<input id="createField" type="text" />')
+  events: {
+    "keypress input" : "saveOnEnter"
+  }
+  
+  saveOnEnter: (evt) -> 
+    if (evt.keyCode == 13)
+      val = $("input").val()
+      v = new Viewing({"address" : val})
+      viewingCollection.create(v)
+  
+  render: ->
+    $(@el).html(@template)
+    @
   
 class ViewingListView extends Backbone.View
   el: "<ul>"
-  render: ->
+  
+  initialize: ->
+    viewingCollection.bind('add', @addViewingView)
+    
+  addViewingView: (item) =>     
+    view = new ViewingView({model: item})
+    $(@el).append(view.render().el)
       
-  renderList: (items) ->
-    items.each (item) ->
-      view = new ViewingView({model: item})
-      $('#insert').append(view.render().el)
+  renderList: (items) =>
+    items.each(@addViewingView)
+    @
     
 
 class ViewingView extends Backbone.View
-  el: "<li>"
+  el: "<li data-name='address'>"
+  template: _.template('[id: <%= id %>] Address: <span data-name="content"> <%= address %></span> <input type="text" class="edit_element" value="<%= address %>" />')
+  initialize: ->
+    _.bindAll(@, 'render')
+    @model.bind('change', @render)
+    
+  events: {
+    "dblclick span[data-name=content]" : "edit",
+    "keypress li input"                : "updateOnEnter"
+  }
   
-  template: _.template('[id: <%= id %>] Address: <span data-name="content"> <%= address %></span>')
+  edit: (evt) ->
+    $(evt.target).hide()
+    $(evt.target).next("input").show()
   
+  updateOnEnter: (evt) ->
+    if (evt.keyCode == 13)
+      attr = $(evt.target).closest("li").attr("data-name");
+      send = {};
+      send[attr] = $(evt.target).val();
+      @model.save(send);
+      $(evt.target).hide();
+      $(evt.target).prev("[data-name=content]").show();
+    
   render: ->
     $(@el).html(@template(@model.toJSON()))
     @
@@ -30,10 +85,14 @@ class AppRouter extends Backbone.Router
   }
   
   index: ->
-    list = new ViewingListView()
-    collection = new ViewingCollection();
-    collection.fetch({success: list.renderList})
-
-
+    mainview = new ViewingIndexView()
+    viewingCollection.fetch({success: (param) ->
+      mainview.setCollection(param)
+      mainview.render()
+      })
+  
+  
+console.log(TrimPath.parseDOMTemplate(""))
+viewingCollection = new ViewingCollection()
 app = new AppRouter()
 Backbone.history.start()
--- public/test.js
+++ public/test.js
@@ -1,5 +1,5 @@
 (function() {
-  var AppRouter, Viewing, ViewingCollection, ViewingListView, ViewingView, app;
+  var AppRouter, Viewing, ViewingCollection, ViewingFormView, ViewingIndexView, ViewingListView, ViewingView, app, viewingCollection;
   var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
     function ctor() { this.constructor = child; }
@@ -7,7 +7,7 @@
     child.prototype = new ctor;
     child.__super__ = parent.prototype;
     return child;
-  };
+  }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   Viewing = (function() {
     __extends(Viewing, Backbone.Model);
     function Viewing() {
@@ -24,21 +24,73 @@
     ViewingCollection.prototype.url = "/viewings";
     return ViewingCollection;
   })();
+  ViewingIndexView = (function() {
+    __extends(ViewingIndexView, Backbone.View);
+    function ViewingIndexView() {
+      ViewingIndexView.__super__.constructor.apply(this, arguments);
+    }
+    ViewingIndexView.prototype.setCollection = function(items) {
+      this.items = items;
+    };
+    ViewingIndexView.prototype.render = function() {
+      var form, list;
+      form = new ViewingFormView();
+      list = new ViewingListView();
+      this.renderPage(form, list);
+      return this;
+    };
+    ViewingIndexView.prototype.renderPage = function(form, list) {
+      $("body").append(form.render().el);
+      return $("body").append(list.renderList(this.items).el);
+    };
+    return ViewingIndexView;
+  })();
+  ViewingFormView = (function() {
+    __extends(ViewingFormView, Backbone.View);
+    function ViewingFormView() {
+      ViewingFormView.__super__.constructor.apply(this, arguments);
+    }
+    ViewingFormView.prototype.template = _.template('<input id="createField" type="text" />');
+    ViewingFormView.prototype.events = {
+      "keypress input": "saveOnEnter"
+    };
+    ViewingFormView.prototype.saveOnEnter = function(evt) {
+      var v, val;
+      if (evt.keyCode === 13) {
+        val = $("input").val();
+        v = new Viewing({
+          "address": val
+        });
+        return viewingCollection.create(v);
+      }
+    };
+    ViewingFormView.prototype.render = function() {
+      $(this.el).html(this.template);
+      return this;
+    };
+    return ViewingFormView;
+  })();
   ViewingListView = (function() {
     __extends(ViewingListView, Backbone.View);
     function ViewingListView() {
+      this.renderList = __bind(this.renderList, this);
+      this.addViewingView = __bind(this.addViewingView, this);
       ViewingListView.__super__.constructor.apply(this, arguments);
     }
     ViewingListView.prototype.el = "<ul>";
-    ViewingListView.prototype.render = function() {};
-    ViewingListView.prototype.renderList = function(items) {
-      return items.each(function(item) {
-        var view;
-        view = new ViewingView({
-          model: item
-        });
-        return $('#insert').append(view.render().el);
+    ViewingListView.prototype.initialize = function() {
+      return viewingCollection.bind('add', this.addViewingView);
+    };
+    ViewingListView.prototype.addViewingView = function(item) {
+      var view;
+      view = new ViewingView({
+        model: item
       });
+      return $(this.el).append(view.render().el);
+    };
+    ViewingListView.prototype.renderList = function(items) {
+      items.each(this.addViewingView);
+      return this;
     };
     return ViewingListView;
   })();
@@ -47,8 +99,31 @@
     function ViewingView() {
       ViewingView.__super__.constructor.apply(this, arguments);
     }
-    ViewingView.prototype.el = "<li>";
-    ViewingView.prototype.template = _.template('[id: <%= id %>] Address: <span data-name="content"> <%= address %></span>');
+    ViewingView.prototype.el = "<li data-name='address'>";
+    ViewingView.prototype.template = _.template('[id: <%= id %>] Address: <span data-name="content"> <%= address %></span> <input type="text" class="edit_element" value="<%= address %>" />');
+    ViewingView.prototype.initialize = function() {
+      _.bindAll(this, 'render');
+      return this.model.bind('change', this.render);
+    };
+    ViewingView.prototype.events = {
+      "dblclick span[data-name=content]": "edit",
+      "keypress li input": "updateOnEnter"
+    };
+    ViewingView.prototype.edit = function(evt) {
+      $(evt.target).hide();
+      return $(evt.target).next("input").show();
+    };
+    ViewingView.prototype.updateOnEnter = function(evt) {
+      var attr, send;
+      if (evt.keyCode === 13) {
+        attr = $(evt.target).closest("li").attr("data-name");
+        send = {};
+        send[attr] = $(evt.target).val();
+        this.model.save(send);
+        $(evt.target).hide();
+        return $(evt.target).prev("[data-name=content]").show();
+      }
+    };
     ViewingView.prototype.render = function() {
       $(this.el).html(this.template(this.model.toJSON()));
       return this;
@@ -64,15 +139,19 @@
       "": "index"
     };
     AppRouter.prototype.index = function() {
-      var collection, list;
-      list = new ViewingListView();
-      collection = new ViewingCollection();
-      return collection.fetch({
-        success: list.renderList
+      var mainview;
+      mainview = new ViewingIndexView();
+      return viewingCollection.fetch({
+        success: function(param) {
+          mainview.setCollection(param);
+          return mainview.render();
+        }
       });
     };
     return AppRouter;
   })();
+  console.log(TrimPath.parseDOMTemplate(""));
+  viewingCollection = new ViewingCollection();
   app = new AppRouter();
   Backbone.history.start();
 }).call(this);
